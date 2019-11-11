15ad09bc53f0ddb0ff244ea3feb32681244bcf4a
robin <playwarrior2@gmail.com>
2019-11-11 16:55:02 +0100

    Shut up and take my money

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './core/app/app.component'
+import { UsecasesComponent } from './about/usecases/usecases.component'
+import { NavbarComponent } from './core/navbar/navbar.component'
+import { UsecaseComponent } from './about/usecases/usecase/usecase.component'
+import { RouterModule } from '@angular/router'
+import { DashboardComponent } from './core/dashboard/dashboard.component'
+import { UsecaseDetailsComponent } from './about/usecases/usecase-details/usecase-details.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsecasesComponent,
+    UsecaseComponent,
+    DashboardComponent,
+    UsecaseDetailsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, RouterModule, NgbModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

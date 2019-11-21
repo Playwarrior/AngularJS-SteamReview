2fcae515694097bb626abe211d2eda9713c1a4c8
robin <playwarrior2@gmail.com>
2019-11-21 21:25:20 +0100

    Update

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const compression = require('compression');
+
+const app = express();
+
+app.use(compression());
+
+const appName = 'AngularJS-SteamReview';
+
+let p = path.join(__dirname, '..', 'dist', appName);
+
+app.use(express.static(p));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', appname, 'index.html'));
+});
+
+const port = process.env.PORT || '4201';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Angular app \'${appName}\' running on port ${port}`));
